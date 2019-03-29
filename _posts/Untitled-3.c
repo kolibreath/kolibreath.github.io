91b20dd78834544ee77aa2d61433b3ff46d762a3
kolibreath <734780178@qq.com>
2019-03-29 09:08:48 +0800

    [ADD] remove *.c

--- _posts/Untitled-3.c
+++
@@ -1,9 +0,0 @@
-sszie_t rio_readn(int fd,void *usrbuf,size_t n){
-    size_t nleft = n;
-    sszie_t nread;
-    char * bufp = usrbuf;
-
-    while(nleft > 0){
-        if((nread = read(fd,bufp,nleft) < 0))
-    }
-}
